2a5e21d03059c0bc0c2fb4de3aecb17c2c664581
nitantchhajed <nitant.chhajed@outlook.com>
2023-07-13 10:58:16 +0530

    Beautify the code

--- src/components/pages/landing/Home.js
+++ src/components/pages/landing/Home.js
@@ -4,11 +4,13 @@ import { Form, Button } from "react-bootstrap";
 import { Ethereum } from 'react-web3-icons';
 import { useState, useEffect } from 'react';
 import { challengeABI } from '../../../challengeContract';
-import { useAccount, useNetwork, useConnect, useSwitchNetwork } from "wagmi";
+import { useAccount, useNetwork, useConnect, useSwitchNetwork, useBalance } from "wagmi";
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import Web3 from 'web3';
 import TxTable from './TxTable';
 const Home = () => {
+
+    //============================================================== HOOKS ===============================================================
     const web3 = new Web3(window.ethereum)
     const { address, isConnected } = useAccount();
     const { chain, chains } = useNetwork()
@@ -18,67 +20,19 @@ const Home = () => {
     const [recipient, setRecipient] = useState("")
     const [ethValue, setEthValue] = useState("")
     const [sentTxn, setSentTxn] = useState([])
-    
-    //=========================================================== CONTRACT ============================================================
+    //------------------------------------------------------------------------------------------------------------------------------------
+
+    //=========================================================== CONTRACT ===============================================================
+
     const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS
     const contractABI = challengeABI
     const CONTRACT_INSTANCE = new web3.eth.Contract(contractABI, contractAddress)
-    //=================================================================================================================================
 
-    //=========================================================== SEND FUNDS ==========================================================
-    async function sendETH() {
-        try {
-            const toWaiValue = toWei(ethValue)
-            console.log(toWaiValue, address);
-            const send = await CONTRACT_INSTANCE.methods.sendFunds(recipient).send({ from: address, value: toWaiValue })
-            console.log({ send });
+    //------------------------------------------------------------------------------------------------------------------------------------
 
-        } catch (error) {
-            console.log(error);
-        }
-    }
 
-    // async function challengeTxn() {
-    //     const challenge = await CONTRACT_INSTANCE.methods.revertTransaction(id).send({ from: WalletAddress })
-    //     console.log({ challenge });
-    // }
-
-    // async function claim() {
-    //     const claimTxn = await CONTRACT_INSTANCE.methods.claimTransaction(id).send({ from: WalletAddress })
-    //     console.log({claimTxn});
-    // }
-
-    const sender = async () => {
-        const senderData = await CONTRACT_INSTANCE.methods.viewTransactionsBySender().call({ from: address })
-        let sentTxnData = []
-        for (let index = 0; index < senderData.length; index++) {
-            const element = senderData[index];
-            const TxnData = await CONTRACT_INSTANCE.methods.allTransactions(element).call({ from: address })
-            sentTxnData.push(TxnData)
-        }
-        
-        setSentTxn(sentTxnData)
-        console.log("sentTxnData", sentTxnData);
-    }
-
-    const received = async () => {
-        const receiverData = await CONTRACT_INSTANCE.methods.viewTransactionsByRecipient().call({ from: address })
-        let receivedTxnData = []
-        for (let index = 0; index < receiverData.length; index++) {
-            const element = receiverData[index];
-            const TxnData = await CONTRACT_INSTANCE.methods.allTransactions(element).call({ from: address })
-            receivedTxnData.push(TxnData)
-            // setSentTxn((oldData)=>[TxnData, ...oldData])
-        }
-
-        console.log("receivedTxnData", receivedTxnData);
-    }
-
-    useEffect(() => {
-        sender()
-        received()
-    }, [])
-
+    //=========================================================== UTILITY FUNCTIONS ===============================================================
+    // Switch Network Hook
     const { switchNetwork } = useSwitchNetwork({
         throwForSwitchChainNotSupported: true,
         onError(error) {
@@ -94,25 +48,100 @@ const Home = () => {
             console.log('Success', data)
         },
     })
+
+    // Switch network function
     const handleSwitch = () => {
         switchNetwork(90001)
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    // convert value to Wei
     function toWei(tokenAmount) {
         return (Web3.utils.toWei(tokenAmount, "ether"));
     }
+
+    //Use Effect - fetching the Sent and received Transaction Data Array
+    useEffect(() => {
+        sender()
+        received()
+    }, [])
+
+    //------------------------------------------------------------------------------------------------------------------------------------
+
+
+    //=========================================================== SEND FUNDS =============================================================
+
+    async function sendETH() {
+        try {
+            const toWaiValue = toWei(ethValue)
+            console.log(toWaiValue, address);
+            const send = await CONTRACT_INSTANCE.methods.sendFunds(recipient).send({ from: address, value: toWaiValue })
+            console.log({ send });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    //=========================================================== REVERT TRANSACTION =============================================================
+
+    async function challengeTxn() {
+        const challenge = await CONTRACT_INSTANCE.methods.revertTransaction(id).send({ from: WalletAddress })
+        console.log({ challenge });
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    //=========================================================== CLAIM TRANSACTION =============================================================
+
+    async function claim() {
+        const claimTxn = await CONTRACT_INSTANCE.methods.claimTransaction(id).send({ from: WalletAddress })
+        console.log({ claimTxn });
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    //=========================================================== SENDER TRANSACTION DATA =============================================================
+
+    const sender = async () => {
+        const senderData = await CONTRACT_INSTANCE.methods.viewTransactionsBySender().call({ from: address })
+        let sentTxnData = []
+        for (let index = 0; index < senderData.length; index++) {
+            const element = senderData[index];
+            const TxnData = await CONTRACT_INSTANCE.methods.allTransactions(element).call({ from: address })
+            sentTxnData.push(TxnData)
+        }
+
+        setSentTxn(sentTxnData)
+        console.log("sentTxnData", sentTxnData);
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    //=========================================================== RECEIVER TRANSACTION DATA =============================================================
+
+    const received = async () => {
+        const receiverData = await CONTRACT_INSTANCE.methods.viewTransactionsByRecipient().call({ from: address })
+        let receivedTxnData = []
+        for (let index = 0; index < receiverData.length; index++) {
+            const element = receiverData[index];
+            const TxnData = await CONTRACT_INSTANCE.methods.allTransactions(element).call({ from: address })
+            receivedTxnData.push(TxnData)
+        }
+        console.log("receivedTxnData", receivedTxnData);
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
     return (
         <>
             <section className='home_wrap'>
@@ -157,7 +186,7 @@ const Home = () => {
                     {!isConnected ? <Button className='btn challenge_btn' onClick={() => connect()}>Connect Wallet</Button> : chain.id !== 90001 ? <button className='btn challenge_btn' onClick={handleSwitch}>Switch to RACE Testnet</button> : <Button className='btn challenge_btn' onClick={sendETH}>Send</Button>}
                 </div>
             </section>
-            <TxTable sentTxn={sentTxn}/>
+            <TxTable sentTxn={sentTxn} />
         </>
     )
 }
